16c023f874a7dae7676562f80f4c68423c1d380f
Shashank Neelagiri <sneelagiri@outlook.com>
2020-01-29 10:15:02 +0100

    react-app starter files

---
+++ src/store/rootReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+export default combineReducers({
+  havingFun: havingFunReducer
+  // we can add more "slice" subreducers here later on...
+});
+function havingFunReducer(state = "yes", action) {
+  switch (action.type) {
+    case "SET_HAVING_FUN": {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
